57700270897606bb698ca43ea0abba021eff60d6
advait-git <advaittiwari2@gmail.com>
2024-02-23 22:56:00 +0530

    basic input sum

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args){
-      
+      Scanner sc = new Scanner(System.in);
+      String name = sc.next();
+      System.out.println("My name is "+name);  
     }
 }
+//we can use nextLine(); or we can use nextInt() or nextFloat();
