2082a8ce0d6033705a3c9718231a4d85add83528
advait-git <advaittiwari2@gmail.com>
2024-02-24 17:39:25 +0530

    add

--- src/reverseString2.java
+++ src/reverseString2.java
@@ -1,28 +1,32 @@
 import java.util.*;
+
 //abcdefg => bacdfeg , k=2 ,n=7
 
-public class reverseString2 {
+public class reverseString2{
     public static String swap(char[] ch,int i,int j){
         char temp=ch[i];
         ch[i]=ch[j];
         ch[j]=temp;
         i++;
         j--; 
-
+    
         return new String(ch); 
      }
-    public static void main(String s,int k){
-
+    public static void main(String [] args){
+    
         //input string
+        // Hiibjbbjv
+        // If string is coming with spaces so take input with sc.nectLine()
+        // If string is coming without spaces so sc.next()
         Scanner sc = new Scanner(System.in);
-        s=sc.nextLine();
-        k=sc.nextInt();
+        String s=sc.nextLine();
+        int k=sc.nextInt();
         //convert the string to char
         char[] ch = new char[s.length()];
         for(int i=0;i<s.length();i++){
             ch[i]=s.charAt(i);
         }
-
+    
         int i=0;
         int n=ch.length;
         while(i<n){
