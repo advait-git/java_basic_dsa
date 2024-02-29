44952736c37ed975fd0306297dd7230d78a1642c
advait-git <advaittiwari2@gmail.com>
2024-02-29 22:58:11 +0530

    avg of an array

---
+++ src/avg.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class avg {
+    public static void main(String[] args){
+        int sum=0;
+
+        int[] nums={1,2,3,4,5,6};
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int avg=sum/nums.length;
+        System.out.print(avg);
+    }
+}
