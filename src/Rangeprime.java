65a0b8c1ec4726dcace3571ecbea833eb9653288
advait-git <advaittiwari2@gmail.com>
2024-03-07 23:49:01 +0530

    in range prime

---
+++ src/Rangeprime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Rangeprime {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int lo=sc.nextInt();
+        int hi=sc.nextInt();
+        for(int i=lo;i<=hi;i++){
+            int count=0;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    count++;
+                    break;
+                }
+            }
+
+           if(count==0){
+             System.out.print(i+" ");
+            }
+
+        }
+    }
+}
