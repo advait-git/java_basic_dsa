917f68172580a3cf15e7684e0e4a4784566341a8
advait-git <advaittiwari2@gmail.com>
2024-03-07 23:17:52 +0530

    prime

---
+++ src/Main_a.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_a {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n=sc.nextInt();
+            int count=0;
+            for(int idx=1;idx<=n;idx++){
+                if(n%idx==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                System.out.println("prime");
+            }else{
+                System.out.println("not prime");
+            }
+        }
+
+    }
+}
