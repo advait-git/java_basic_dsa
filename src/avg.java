f22b6ab69042d6b5733d1147570dca249dc69357
advait-git <advaittiwari2@gmail.com>
2024-03-08 17:34:43 +0530

    java

--- src/avg.java
+++ src/avg.java
@@ -5,7 +5,7 @@ public class avg {
     public static void main(String[] args){
         int sum=0;
 
-        int[] nums={1,2,3,4,5,6};
+        int[] nums={1,2,3,4,5,19};
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
