8e4719b88d84f2b8d8f52cab5103fabf734dec90
Somendra Tiwari <tiwarisomendra22@gmail.com>
2025-04-24 02:15:07 +0530

    added toh

--- src/recurtion6.java
+++ src/recurtion6.java
@@ -1,7 +1,19 @@
+
+import java.util.Scanner;
+
 public class recurtion6 {
     public static void main(String [] str){
         Scanner sc = new Scanner (System.in);
-        int x=sc.nextInt();
+        int n=sc.nextInt();
+        int t1d=sc.nextInt();
+        int t2d = sc.nextInt();
+        int t3d = sc.nextInt();
+        toh(n,t1d , t2d,t3d);
     }
-    public static 
+    public static void toh(int n,int t1id,int t2id , int t3id){
+        if (n==0) return;
+        toh(n-1,t1id,t3id,t2id);
+        System.out.println(n+" "+t1id+" "+t2id);
+        toh(n-1,t3id,t2id,t1id);
+    }
 }
